f51947e4ac201b19362a00cbdc72c3919a00930d
Nikolay Gorbachev <n1kko.gorbachevv@gmail.com>
2023-12-30 18:55:24 +0300

    Initial commit

---
+++ EF.Start/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EF.Start
+{
+    internal class Program()
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
